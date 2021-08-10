0dd57a5e3b741da71b62d6251d99cf96e1324eda
CrazyMax <contact@crazymax.dev>
2021-08-10 08:42:33 +0200

    Use `getBooleanInput` (#101)
    Co-authored-by: CrazyMax <crazy-max@users.noreply.github.com>

--- src/context.ts
+++ src/context.ts
@@ -17,10 +17,10 @@ export async function getInputs(): Promise<Inputs> {
   return {
     gpgPrivateKey: core.getInput('gpg-private-key', {required: true}),
     passphrase: core.getInput('passphrase'),
-    gitUserSigningkey: /true/i.test(core.getInput('git-user-signingkey')),
-    gitCommitGpgsign: /true/i.test(core.getInput('git-commit-gpgsign')),
-    gitTagGpgsign: /true/i.test(core.getInput('git-tag-gpgsign')),
-    gitPushGpgsign: /true/i.test(core.getInput('git-push-gpgsign')),
+    gitUserSigningkey: core.getBooleanInput('git-user-signingkey'),
+    gitCommitGpgsign: core.getBooleanInput('git-commit-gpgsign'),
+    gitTagGpgsign: core.getBooleanInput('git-tag-gpgsign'),
+    gitPushGpgsign: core.getBooleanInput('git-push-gpgsign'),
     gitCommitterName: core.getInput('git-committer-name'),
     gitCommitterEmail: core.getInput('git-committer-email'),
     workdir: core.getInput('workdir') || '.'
