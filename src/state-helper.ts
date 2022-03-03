11e26b9f0bb18109d1d726532d7c93cd0b0d5a19
Jose Celano <josecelano@gmail.com>
2022-03-03 13:48:37 +0000

    Fix error deleting keys with short key id (#129)
    Error:
    
    ```
    gpg: can't do this in batch mode
    ```
    
    The GPG command:
    
    ```
    gpg --batch --yes --delete-secret-keys FINGERPRINT
    ```
    
    requires to use a fingerprint if you use the `--batch` option.
    
    We were using the short ID of the primary key.

--- src/state-helper.ts
+++ src/state-helper.ts
@@ -2,14 +2,9 @@ import * as core from '@actions/core';
 
 export const IsPost = !!process.env['STATE_isPost'];
 export const fingerprint = process.env['STATE_fingerprint'] || '';
-export const keyId = process.env['STATE_keyId'] || '';
 
 export function setFingerprint(fingerprint: string) {
   core.saveState('fingerprint', fingerprint);
-}
-
-export function setKeyID(keyID: string) {
-  core.saveState('keyId', keyID);
 }
 
 if (!IsPost) {
