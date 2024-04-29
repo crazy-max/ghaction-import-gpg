ed417001355423ca89cd8d56e7338965f9d9daea
Florian Friedrich <ffried@me.com>
2024-04-29 11:11:31 +0200

    Allow using any user id of the key's user ids as committer identity. Fixes #156

--- tests/openpgp.test.ts
+++ tests/openpgp.test.ts
@@ -12,8 +12,16 @@ const userInfos = [
     pgp: fs.readFileSync(path.join(fixturesDir, 'test-key.pgp'), {encoding: 'utf8', flag: 'r'}),
     pgp_base64: fs.readFileSync(path.join(fixturesDir, 'test-key-base64.pgp'), {encoding: 'utf8', flag: 'r'}),
     passphrase: fs.readFileSync(path.join(fixturesDir, 'test-key.pass'), {encoding: 'utf8', flag: 'r'}),
-    name: 'Joe Tester',
-    email: 'joe@foo.bar',
+    primaryUserId: {
+      name: 'Joe Tester',
+      email: 'joe@foo.bar'
+    },
+    userIds: [
+      {
+        name: 'Joe Tester',
+        email: 'joe@foo.bar'
+      }
+    ],
     keyID: '7D851EB72D73BDA0',
     fingerprint: '27571A53B86AF0C799B38BA77D851EB72D73BDA0',
     keygrip: '3E2D1142AA59E08E16B7E2C64BA6DDC773B1A627'
@@ -23,8 +31,16 @@ const userInfos = [
     pgp: fs.readFileSync(path.join(fixturesDir, 'test-subkey.pgp'), {encoding: 'utf8', flag: 'r'}),
     pgp_base64: fs.readFileSync(path.join(fixturesDir, 'test-subkey-base64.pgp'), {encoding: 'utf8', flag: 'r'}),
     passphrase: fs.readFileSync(path.join(fixturesDir, 'test-subkey.pass'), {encoding: 'utf8', flag: 'r'}),
-    name: 'Joe Bar',
-    email: 'joe@bar.foo',
+    primaryUserId: {
+      name: 'Joe Bar',
+      email: 'joe@bar.foo'
+    },
+    userIds: [
+      {
+        name: 'Joe Bar',
+        email: 'joe@bar.foo'
+      }
+    ],
     keyID: '6071D218380FDCC8',
     fingerprint: '87F257B89CE462100BEC0FFE6071D218380FDCC8',
     keygrips: ['F5C3ABFAAB36B427FD98C4EDD0387E08EA1E8092', 'DEE0FC98F441519CA5DE5D79773CB29009695FEB']
@@ -38,16 +54,22 @@ for (const userInfo of userInfos) {
       it('returns a PGP private key from an armored string', async () => {
         await openpgp.readPrivateKey(userInfo.pgp).then(privateKey => {
           expect(privateKey.keyID).toEqual(userInfo.keyID);
-          expect(privateKey.name).toEqual(userInfo.name);
-          expect(privateKey.email).toEqual(userInfo.email);
+          expect(privateKey.primaryUserId.name).toEqual(userInfo.primaryUserId.name);
+          expect(privateKey.primaryUserId.email).toEqual(userInfo.primaryUserId.email);
+          expect(privateKey.allUserIds).toHaveLength(userInfo.userIds.length);
+          expect(privateKey.allUserIds[0].name).toEqual(userInfo.userIds[0].name);
+          expect(privateKey.allUserIds[0].email).toEqual(userInfo.userIds[0].email);
           expect(privateKey.fingerprint).toEqual(userInfo.fingerprint);
         });
       });
       it('returns a PGP private key from a base64 armored string', async () => {
         await openpgp.readPrivateKey(userInfo.pgp_base64).then(privateKey => {
           expect(privateKey.keyID).toEqual(userInfo.keyID);
-          expect(privateKey.name).toEqual(userInfo.name);
-          expect(privateKey.email).toEqual(userInfo.email);
+          expect(privateKey.primaryUserId.name).toEqual(userInfo.primaryUserId.name);
+          expect(privateKey.primaryUserId.email).toEqual(userInfo.primaryUserId.email);
+          expect(privateKey.allUserIds).toHaveLength(userInfo.userIds.length);
+          expect(privateKey.allUserIds[0].name).toEqual(userInfo.userIds[0].name);
+          expect(privateKey.allUserIds[0].email).toEqual(userInfo.userIds[0].email);
           expect(privateKey.fingerprint).toEqual(userInfo.fingerprint);
         });
       });
@@ -55,7 +77,7 @@ for (const userInfo of userInfos) {
 
     describe('generateKeyPair', () => {
       it('generates a PGP key pair', async () => {
-        await openpgp.generateKeyPair(userInfo.name, userInfo.email, userInfo.passphrase).then(keyPair => {
+        await openpgp.generateKeyPair(userInfo.primaryUserId.name, userInfo.primaryUserId.email, userInfo.passphrase).then(keyPair => {
           expect(keyPair).not.toBeUndefined();
           expect(keyPair.publicKey).not.toBeUndefined();
           expect(keyPair.privateKey).not.toBeUndefined();
